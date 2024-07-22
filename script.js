5cade23a2caa275224ea37785ae568d51dd8938c
Muhammad Talha Naeem <mt8442905@gmail.com>
2024-07-22 15:58:36 +0500

    kmdc

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var books;
+var loadedBooksIndex = 4;
 $(document).ready(function () {
+
+    showinitialbooks();
+    
+
+
     $('#search-box').focus(function () { 
         $('#search-container').css('visibility', 'visible');
         $('#Add-Book-container').css('visibility', 'hidden');
-        //document.getElementById('search-container').style.visibility = 'visible';
     });
+
+
     $('#search-box').blur(function () { 
         $('#search-container').css('visibility', 'hidden');
-        //document.getElementById('search-container').style.visibility = 'hidden';
     });
+
+
     $('#search-box').keyup(function (event) { 
-        //Content changes, do search
         $('#searched-books').html("");
         var keyword = $(this).val();
         for(let i=0; i<books.length; i++)
@@ -25,13 +32,12 @@ $(document).ready(function () {
                 addBookInSearchList(books[i].title, books[i].author, books[i].imageUrl);
             }
         }
-
     });
+
+
+
     $('#view-books').click(function () { 
-        for(let i=5; i<books.length; i++)
-        {
-            addBookInList(books[i].title, books[i].author, books[i].price, books[i].imageUrl);
-        }
+        loadRemainingBooks();
         $(this).attr('disabled', 'true');
     });
     $('#add-btn-nav').click(function () { 
@@ -42,23 +48,34 @@ $(document).ready(function () {
         $('#Add-Book-container').css('visibility', 'hidden');
         
     });
-    $.ajax({
-        type: "GET",
-        url: "https://localhost:7015/api/books",
-        dataType: "json",
-        success: function (data) {
-            books = data;
-            for(let i=0; i<5; i++)
-            {
-                addBookInList(books[i].title, books[i].author, books[i].price, books[i].imageUrl);
-            }
-        },
-        error: function(error){
-            console.log("Some Error occured");
-        }
-    });
     
+
+
+
+    $('#add-book-form').submit(function (e) { 
+        e.preventDefault();
+        var formData = { id : $('#book-id').val(), title: $('#book-title').val(), author: $('#book-author').val(), price: $('#book-price').val(),  imageUrl: $('#book-imageurl').val(),}
+        $.ajax({
+            type: "POST",
+            contentType : "application/json",
+            url: "https://localhost:7015/api/books",
+            data: JSON.stringify(formData),
+            success: function(){
+                $('#Add-Book-container').css('visibility', 'hidden');
+                shownotif("The book is added to library", 3);
+                $('#add-book-form').trigger("reset");
+                updateBooklist();
+            },
+            error: function(error){
+                shownotif("Book Server Connection Error",3);
+                $('#Add-Book-container').css('visibility', 'hidden');
+            }
+          });
+        
+    });
 });
+
+
 function addBookInSearchList(title, author, imgSource) {
     var newElem = document.createElement('div');
     newElem.className = "searched-book";
@@ -70,6 +87,10 @@ function addBookInSearchList(title, author, imgSource) {
         
     document.getElementById('searched-books').appendChild(newElem);
 }
+
+
+
+
 function addBookInList(title, author, price, imgSource) {
     var newElem = document.createElement('tr');
     newElem.innerHTML = `
@@ -84,4 +105,75 @@ function addBookInList(title, author, price, imgSource) {
         </td>
         `;
     document.getElementsByTagName('table')[0].appendChild(newElem);
+}
+
+
+
+
+async function shownotif(message, time)
+{
+    $('#notification-area').html(message);
+    $('#notification-area').css('visibility', 'visible');
+    await new Promise(resolve => setTimeout(resolve, time*1000));
+    $('#notification-area').css('visibility', 'hidden');
+}
+
+
+
+
+function loadInitialBooks()
+{
+    for(let i=0; i<5; i++)
+    {
+        addBookInList(books[i].title, books[i].author, books[i].price, books[i].imageUrl);
+    }
+}
+
+
+
+
+function loadRemainingBooks()
+{
+    for(let i=loadedBooksIndex+1; i<books.length; i++)
+    {
+        addBookInList(books[i].title, books[i].author, books[i].price, books[i].imageUrl);
+        loadedBooksIndex++;
+    }
+}
+
+
+
+
+function updateBooklist()
+{
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:7015/api/books",
+        dataType: "json",
+        success: function (data) {
+            books = data;
+            loadRemainingBooks();
+        },
+        error: function(error){
+            shownotif("Book Server Connection Error",3);
+        }
+    });
+}
+
+
+
+function showinitialbooks()
+{
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:7015/api/books",
+        dataType: "json",
+        success: function (data) {
+            books = data;
+            loadInitialBooks();
+        },
+        error: function(error){
+            shownotif("Book Server Connection Error",3);
+        }
+    });
 }
